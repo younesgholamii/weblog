7c6fd833c569646637e3569691bce4414fa31cea
younes <younes3021.us@gmail.com>
2024-12-28 10:53:00 +0330

    initial commit

--- weblog/models.py
+++ weblog/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Article(models.Model):
+    publisher = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    categories = models.ManyToManyField("Category", verbose_name='categories')
+    title = models.CharField(max_length=110, verbose_name='article title')
+    text = models.TextField()
+    slug = models.SlugField(blank=True, null=True, verbose_name='article slug')
+    image = models.ImageField(verbose_name='article image', upload_to='weblog/articles/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='created at')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='updated at')
+
+    def __str__(self):
+        return f"Article {self.title}"
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=110)
+
+    def __str__(self):
+        return self.title
+    
+
