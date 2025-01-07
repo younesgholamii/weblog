d869ab94ac84506ce7bf8bad464a78279c352670
younes <younes3021.us@gmail.com>
2025-01-07 11:01:09 +0330

    POST method added

--- weblog/serializers.py
+++ weblog/serializers.py
@@ -1,11 +1,19 @@
 from rest_framework import serializers
-
+from django.contrib.auth.models import User
+from .models import Article
 
 class ArticleSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
+    id = serializers.IntegerField(required=False)
     title = serializers.CharField(max_length=110)
     text = serializers.CharField()
-    image = serializers.ImageField()
-    slug = serializers.SlugField()
-    created_at = serializers.DateTimeField()
-    updated_at = serializers.DateTimeField()
+    image = serializers.ImageField(required=False)
+    slug = serializers.SlugField(required=False)
+    created_at = serializers.DateTimeField(required=False)
+    updated_at = serializers.DateTimeField(required=False)
+    publisher = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
+    categories = serializers.PrimaryKeyRelatedField(read_only=True, many=True)
+    
+    def create(self, validated_data):
+        article = Article.objects.create(**validated_data)
+        return article
+
--- weblog/views.py
+++ weblog/views.py
@@ -1,15 +1,27 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
-from .models import Article
+from .models import Article, Category
 from .serializers import ArticleSerializer
 from rest_framework import status
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def BlogListApi(request):
-    articles = Article.objects.all()
-    serializer = ArticleSerializer(articles, many=True)
-    return Response(serializer.data, status=status.HTTP_200_OK)
+    if request.method == 'GET':
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'POST':
+        serializer = ArticleSerializer(data=request.data)
+        if serializer.is_valid():
+            categories_id = request.data.get('categories')
+            article = serializer.save()
+            for category_id in categories_id:
+                category = get_object_or_404(Category, id=category_id)
+                article.categories.add(category)
+            return Response(data=serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 @api_view(['GET', 'DELETE'])
 def BlogDetailApi(request, article_id):
