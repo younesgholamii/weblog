74f7de0b4cf5c4a69121b79cf02f1672c576aebe
younes <younes3021.us@gmail.com>
2025-01-08 11:52:15 +0330

    PUT method added

--- weblog/views.py
+++ weblog/views.py
@@ -13,23 +13,26 @@ def BlogListApi(request):
         return Response(serializer.data, status=status.HTTP_200_OK)
     elif request.method == 'POST':
         serializer = ArticleSerializer(data=request.data)
-        if serializer.is_valid():
-            categories_id = request.data.get('categories')
-            article = serializer.save()
-            for category_id in categories_id:
-                category = get_object_or_404(Category, id=category_id)
-                article.categories.add(category)
-            return Response(data=serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(data=serializer.data, status=status.HTTP_201_CREATED)
 
 
-@api_view(['GET', 'DELETE'])
+@api_view(['GET', 'DELETE', 'PUT'])
 def BlogDetailApi(request, article_id):
     article = get_object_or_404(Article, id=article_id)
     if request.method == 'GET':
         serializer = ArticleSerializer(article)
         return Response(data=serializer.data, status=status.HTTP_200_OK)
+    
     elif request.method == 'DELETE':
         article.delete()
         result = {"result" : 'this article has deleted'}
         return Response(result, status=status.HTTP_204_NO_CONTENT)
+    
+    elif request.method == 'PUT':
+        serializer = ArticleSerializer(article, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
